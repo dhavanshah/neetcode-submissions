e73d0f564002bd3d6a2cf6d4443e8c7fe1784bfb
dhavanshah <74516243+dhavanshah@users.noreply.github.com>
2026-09-12 01:13:35 +0100

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,23 @@
+//Sliding Window
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int buy = 0;
+        int sell = 1;
+        int maxProfit = 0;
+
+        while(sell < prices.length) {
+
+            if(prices[buy] < prices[sell]) {
+                int profit = prices[sell] - prices[buy];
+                maxProfit = Math.max(profit, maxProfit);
+                
+            } else {
+                buy=sell;
+            }
+            sell++;
+        }
+        return maxProfit;
+        
+    }
+}
